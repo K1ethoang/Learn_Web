f8c1b3a3bb5a5cf04932f5a1d0e25e19f9c02631
Kiet Hoang <kiethoang101@gmail.com>
2022-07-04 22:18:48 +0700

    Update

--- practice/Xây dựng Tabs UI/main.js
+++ practice/Xây dựng Tabs UI/main.js
@@ -1,3 +1,26 @@
 const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-console.log(typeof $);
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
+
+const tabActive = $(".tab-item.active");
+const line = $(".tabs .line");
+
+line.style.left = tabActive.offsetLeft + "px";
+line.style.width = tabActive.offsetWidth + "px";
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+
+  tab.onclick = () => {
+    $(".tab-item.active").classList.remove("active");
+    tab.classList.add("active");
+
+    $(".tab-pane.active").classList.remove("active");
+    pane.classList.add("active");
+
+    line.style.left = tab.offsetLeft + "px";
+    line.style.width = tab.offsetWidth + "px";
+  };
+});
